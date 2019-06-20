c422d0d2b300b4d63cfa23a467b38dbb30336f3c
Belmin Ruhotina <44238254+beelmin@users.noreply.github.com>
2019-06-20 20:10:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -35,32 +35,6 @@ app.get('/', function (req, res,next) {
 	
 });
 
-app.get('/chat', function(req,res,next){
-
-
-	const instance = new MongoClient(uri, { useNewUrlParser: true });
-	instance.connect((err, client) => {
-	  if (err){
-	  	console.log('failed to connect')
-	  	console.log(err)
-	  } 
-	  else {
-	    console.log('connected')
-	    const collection = client.db("chat").collection("messages").find({}).toArray(function(err, result) {
-		    if (err) throw err;
-		   
-		    instance.close();
-		    res.json({
-			"conversations": result
-		    });
-		    
-		  });
-	    
-	  }
-	 });
-
-});
-
 
 app.post('/', function(req, res, next) {
 	if(req.body.authentication){
